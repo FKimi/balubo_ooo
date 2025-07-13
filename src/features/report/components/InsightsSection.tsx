2bc76d605dc0c59ca39740d51404758fd8293de4
Fkimi <fuccer134097@gmail.com>
2025-07-13 22:49:47 +0900

    Fix index variable name in InsightsSection map function

--- src/features/report/components/InsightsSection.tsx
+++ src/features/report/components/InsightsSection.tsx
@@ -282,7 +282,7 @@ export function InsightsSection({ works, inputs, workStats }: InsightsSectionPro
           </CardHeader>
           <CardContent className="pt-0">
             <div className="space-y-3">
-              {suggestions.map((suggestion, _index) => (
+              {suggestions.map((suggestion, index) => (
                 <div 
                   key={index} 
                   className={`rounded-lg p-4 border-l-4 ${
